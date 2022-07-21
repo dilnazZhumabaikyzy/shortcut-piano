604c7aa7b3f1455a3de96b0e1ea45a4fc6e9f1a7
Dilnaz Zhumabaikyzy <dilnazzh200303@gmail.com>
2022-07-21 19:03:27 +0600

    ver 3

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import {Header} from './components/header';
 import {Main} from './pages/main-page'
 import {Footer} from './components/footer'
 import { MyRoutes } from './routing';
-
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    axios.get("http://localhost:5000/getBasicGmail").then((response) => {
+      console.log(response.data);
+    });
+  }, []);
+
   return (
   <>
     <div className='container'>
